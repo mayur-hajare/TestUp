63f0e46d8c85c48fd3d26d3a7d19ec81a03693bd
mayur-hajare <myur.hajare@gmail.com>
2021-05-02 19:59:51 +0530

    Splash Screen Done. SignUp And Login Activity Ui Done.

---
+++ app/src/main/java/com/myur/testup/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.myur.testup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView SignUP=findViewById(R.id.SignUP);
+        SignUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/myur/testup/MainActivity.java
+++ app/src/main/java/com/myur/testup/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,DashboardActivity.class);
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
