5f50e10d32c530724138346a04fb32831b621725
mayur-hajare <myur.hajare@gmail.com>
2021-05-03 21:24:59 +0530

    User created on firebase and Login Successfully... RecycleView is created but Image not View...

--- app/src/main/java/com/myur/testup/DashboardActivity.java
+++ app/src/main/java/com/myur/testup/DashboardActivity.java
@@ -50,7 +50,7 @@ public class DashboardActivity extends AppCompatActivity {
                     }
                 };
             recyclerView.setAdapter(firebaseRecyclerAdapter);
-            //Fail
+
 
         }
 
