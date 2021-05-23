bc2519b578ae7a6d8b89f6de79c241f7f21cba52
mayur-hajare <myur.hajare@gmail.com>
2021-05-23 10:56:22 +0530

    RecyclerView and Adapter Changed.... Internet AltDialog...

--- app/src/main/java/com/myur/testup/Common.java
+++ app/src/main/java/com/myur/testup/Common.java
@@ -3,27 +3,34 @@ package com.myur.testup;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class Common {
+    private  Context context;
+    private boolean isconnected=false;
+    private ConnectivityManager connectivityManager;
 
-    public static boolean isConnectedToInternet(Context context) {
+    public Common(Context context){
+        this.context=context;
 
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if (connectivityManager != null) {
-            NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
-            if (info != null) {
-                for (int i = 0; i < info.length; i++) {
+    }
 
-                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
-                        return true;
+    public boolean isInternetConnected(){
+        try{
+            connectivityManager= (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+            isconnected=networkInfo!=null && networkInfo.isAvailable() && networkInfo.isConnected();
+            return isconnected;
 
-                }
+        }catch (Exception ex){
+            Log.e("Error",ex.getLocalizedMessage());
 
-            }
 
         }
-        return false;
+        return isconnected;
+
+
     }
+
 }
--- app/src/main/java/com/myur/testup/DisplayImagesActivity.java
+++ app/src/main/java/com/myur/testup/DisplayImagesActivity.java
@@ -1,84 +1,86 @@
 package com.myur.testup;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.bumptech.glide.load.model.ModelLoader;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DisplayImagesActivity extends AppCompatActivity {
 
-    RecyclerView mrecyclerView,mrecyclerViewv;
+    Common common;
+    boolean isconnected = false;
+    RecyclerView mrecyclerView, mrecyclerViewv;
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference reference,referencenew;
+    DatabaseReference reference, referencenew;
 
-    CheckNetworkListner checkNetworkListner=new CheckNetworkListner();
 
-    FirebaseRecyclerOptions<Member>options;
-    FirebaseRecyclerOptions<Member2>option;
-    FirebaseRecyclerAdapter<Member,ViewHolder>adapter;
-    FirebaseRecyclerAdapter<Member2,ViewHolder>adapterNew;
+    FirebaseRecyclerOptions<Member> options;
+    FirebaseRecyclerOptions<Member2> option;
+    FirebaseRecyclerAdapter<Member, ViewHolder> adapter;
+    FirebaseRecyclerAdapter<Member2, ViewHolder> adapterNew;
+
 
     @SuppressLint("WrongConstant")
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        IntentFilter filter=new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        registerReceiver(checkNetworkListner,filter);
+
+        common = new Common(DisplayImagesActivity.this);
+        isconnected = common.isInternetConnected();
+        if (!isconnected) {
+            AlertDialog alertDialog = new AlertDialog.Builder(DisplayImagesActivity.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setCancelable(true);
+            alertDialog.setMessage("Check Your Internet Connection..!");
+            alertDialog.setIcon(R.drawable.ic_baseline_error);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(DisplayImagesActivity.this,"Make sure Your Connection On...",Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
 
 
         setContentView(R.layout.activity_display_images);
-        mrecyclerViewv=findViewById(R.id.recyclerViewv);
-        mrecyclerView=findViewById(R.id.recyclerViewh);
+        mrecyclerViewv = findViewById(R.id.recyclerViewv);
+        mrecyclerView = findViewById(R.id.recyclerViewh);
         mrecyclerViewv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         mrecyclerViewv.setHasFixedSize(true);
         mrecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         mrecyclerView.setHasFixedSize(true);
-        reference=FirebaseDatabase.getInstance().getReference().child("Products");
-        referencenew=FirebaseDatabase.getInstance().getReference().child("NewData");
+        reference = FirebaseDatabase.getInstance().getReference().child("Products");
+        referencenew = FirebaseDatabase.getInstance().getReference().child("NewData");
         LoadData();
         LoadNewData();
 
     }
-    @Override
-    protected void onStart() {
-    IntentFilter filter=new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-    registerReceiver(checkNetworkListner,filter);
 
-        super.onStart();
-    }
 
     private void LoadNewData() {
-        option=new FirebaseRecyclerOptions.Builder<Member2>().setQuery(referencenew,Member2.class).build();
-        adapterNew=new FirebaseRecyclerAdapter<Member2, ViewHolder>(option) {
+        option = new FirebaseRecyclerOptions.Builder<Member2>().setQuery(referencenew, Member2.class).build();
+        adapterNew = new FirebaseRecyclerAdapter<Member2, ViewHolder>(option) {
             @Override
             protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member2 model) {
                 holder.textView.setText(model.getTitle());
@@ -88,21 +90,22 @@ public class DisplayImagesActivity extends AppCompatActivity {
             @NonNull
             @Override
             public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.image,parent,false);
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.image, parent, false);
 
                 return new ViewHolder(v);
 
             }
-        };adapterNew.startListening();
+        };
+        adapterNew.startListening();
         mrecyclerView.setAdapter(adapterNew);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         mrecyclerView.setLayoutManager(layoutManager);
 
     }
 
     private void LoadData() {
-        options=new  FirebaseRecyclerOptions.Builder<Member>().setQuery(reference,Member.class).build();
-        adapter=new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
+        options = new FirebaseRecyclerOptions.Builder<Member>().setQuery(reference, Member.class).build();
+        adapter = new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
                 holder.textView.setText(model.getTitle());
@@ -113,7 +116,7 @@ public class DisplayImagesActivity extends AppCompatActivity {
             @NonNull
             @Override
             public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.image,parent,false);
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.image, parent, false);
 
                 return new ViewHolder(v);
 
@@ -122,13 +125,19 @@ public class DisplayImagesActivity extends AppCompatActivity {
         };
         adapter.startListening();
         mrecyclerViewv.setAdapter(adapter);
-        StaggeredGridLayoutManager staggeredGridLayoutManager=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         mrecyclerViewv.setLayoutManager(staggeredGridLayoutManager);
     }
 
     @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+
+    @Override
     protected void onStop() {
-        unregisterReceiver(checkNetworkListner);
+
         super.onStop();
     }
 
